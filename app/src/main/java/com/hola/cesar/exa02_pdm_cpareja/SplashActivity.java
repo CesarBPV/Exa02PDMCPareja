b3e11fa1929a32a9625e6bafc5e097f433210ec1
Cesar <cesarpareja@upeu.edu.pe>
2018-04-30 12:07:33 -0500

    Splash activity 2

--- app/src/main/java/com/hola/cesar/exa02_pdm_cpareja/SplashActivity.java
+++ app/src/main/java/com/hola/cesar/exa02_pdm_cpareja/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.hola.cesar.exa02_pdm_cpareja;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -14,11 +17,27 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme2);
         setContentView(R.layout.activity_splash);
         tv=findViewById(R.id.tapp);
         iv=findViewById(R.id.imv);
         Animation myAnim= AnimationUtils.loadAnimation(this,R.anim.mytransition);
         tv.startAnimation(myAnim);
         iv.startAnimation(myAnim);
+        TransitionDrawable transitionb=(TransitionDrawable) findViewById(R.id.spc).getBackground();
+        transitionb.startTransition(2000);
+        Integer colorFrom = getResources().getColor(R.color.colorWhite);
+        Integer colorTo = getResources().getColor(R.color.colorPrimary);
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(2000);
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                tv.setTextColor((Integer)animator.getAnimatedValue());
+            }
+
+        });
+        colorAnimation.start();
     }
 }
